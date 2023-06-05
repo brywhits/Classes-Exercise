309e553be664977a9a42d763faf23b4dcdd157ac
Bryant Whitsett <bryantwhitsett71@gmail.com>
2023-06-05 00:16:43 -0400

    ExerciseOne

---
+++ Classes/Car.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Classes
+{
+	public class Car
+	{
+
+		public Car()
+		{
+
+		}
+
+		public Car(string make, string model, int year)
+		{
+			Make = make;
+			Model = model;
+			Year = year;
+
+		}
+
+		public string Make { get; set; }
+		public string Model { get; set; }
+		public int Year { get; set; }
+
+
+	}
+}
+
--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace Classes
+﻿using System;
+
+namespace Classes
 {
-    public class Program
+    class Programs
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Car myCar = new Car();
+            myCar.Make = "Ford";
+            myCar.Model = "Focus";
+            myCar.Year = 2013;
+
+            var Mazda = new Car()
+            {
+                Make = "Mazda",
+                Model = "CX5",
+                Year = 2016,
+            };
+
+            var chevy = new Car("Chevy", "Impala", 2001);
+
+
+            var carList = new List<Car>() { myCar, Mazda, chevy };
+
+            foreach (var vehicle in carList)
+            {
+                Console.WriteLine($"{ vehicle.Make} {vehicle.Model} {vehicle.Year }");
+            }
         }
     }
 }
